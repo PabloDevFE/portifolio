aa3345e67b00e2e5570ad539d863f292261ab0ad
Pablo Henrique <125980185+PabloDevFE@users.noreply.github.com>
2023-07-24 19:36:51 -0300

    aprimorando

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,13 @@
 const menu = document.querySelector('#menu')
+const buttonMenu = document.querySelector('.menuImg')
 
 
 function exibirMenu() {
     if (menu.classList.value == "menu") {
         menu.classList.replace('menu', 'hidden')
+        buttonMenu.src = "src/midias/icons8-hamburger-menu.svg"
     } else {
         menu.classList.replace('hidden', 'menu')
+        buttonMenu.src = "src/midias/icons8-close.svg"
     }
 }
