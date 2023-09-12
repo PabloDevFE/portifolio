89e599c2d8d37d0692d5a2db6a62945be6829698
Pablo Henrique <125980185+PabloDevFE@users.noreply.github.com>
2023-09-12 12:27:19 -0300

    att
    fazendo ajustes e correções

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,36 +1,31 @@
 let header = document.querySelector("#header");
 let navigation = document.querySelector("#navigation");
+let main = document.querySelector("#main");
 let showSidebar = false;
-function toggleSidebar()
-{
-    showSidebar = !showSidebar;
-    if(showSidebar)
-    {
-        navigation.style.marginLeft = '-5vw';
-        navigation.style.animationName = 'showMenu';
-        content.style.filter = 'blur(2px)';
-    }
-    else
-    {
-        navigation.style.marginLeft = '-100vw';
-        navigation.style.animationName = '';
-        content.style.filter = '';
-    }
+
+function toggleSidebar() {
+  showSidebar = !showSidebar;
+  if (showSidebar) {
+    navigation.style.marginLeft = "-5vw";
+    navigation.style.animationName = "showMenu";
+    main.style.filter = "blur(2px)";
+  } else {
+    navigation.style.marginLeft = "-100vw";
+    navigation.style.animationName = "";
+    main.style.filter = "";
+  }
 }
 
-function closeSidebar()
-{
-    if(showSidebar)
-    {
-        showSidebar = true;
-        toggleSidebar();
-    }
+function closeSidebar() {
+  if (showSidebar) {
+    showSidebar = true;
+    toggleSidebar();
+  }
 }
 
-window.addEventListener('resize', function(event) {
-    if(window.innerWidth > 768 && showSidebar) 
-    {  
-        showSidebar = true;
-        toggleSidebar();
-    }
+window.addEventListener("resize", function (event) {
+  if (window.innerWidth > 768 && showSidebar) {
+    showSidebar = true;
+    toggleSidebar();
+  }
 });
