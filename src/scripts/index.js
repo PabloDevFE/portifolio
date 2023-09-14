99d3c557b9180fdb7c3e472b678357eedb33b869
Pablo Henrique <125980185+PabloDevFE@users.noreply.github.com>
2023-09-14 10:58:45 -0300

    aprimorando o menu mobile

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,14 +1,16 @@
 let header = document.querySelector("#header");
 let navigation = document.querySelector("#navigation");
 let main = document.querySelector("#main");
+let btnOpen = document.querySelector("#btnHeaderHamburguer");
+let btnClose = document.querySelector("#btnHeaderClose");
 let showSidebar = false;
 
 function toggleSidebar() {
   showSidebar = !showSidebar;
   if (showSidebar) {
-    navigation.style.marginLeft = "-5vw";
+    navigation.style.marginLeft = "0vw";
     navigation.style.animationName = "showMenu";
-    main.style.filter = "blur(2px)";
+    main.style.filter = "blur(10px)";
   } else {
     navigation.style.marginLeft = "-100vw";
     navigation.style.animationName = "";
@@ -26,6 +28,17 @@ function closeSidebar() {
 window.addEventListener("resize", function (event) {
   if (window.innerWidth > 768 && showSidebar) {
     showSidebar = true;
+    btnOpen.style.visibility = "";
     toggleSidebar();
   }
+});
+
+btnOpen.addEventListener("click", () => {
+  toggleSidebar();
+  btnOpen.style.visibility = "hidden";
+});
+
+btnClose.addEventListener("click", () => {
+  toggleSidebar();
+  btnOpen.style.visibility = "";
 });
